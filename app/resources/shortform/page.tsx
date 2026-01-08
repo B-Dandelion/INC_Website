f25b002fa5324e700caff3944093cefc2d8f1e4c
B-Dandelion <jinbabging@gmail.com>
2026-01-08 16:10:07 +0900

    6차: resources '자료실' / 하위 카테고리 매핑

---
+++ app/resources/shortform/page.tsx
@@ -0,0 +1,10 @@
+import PageShell from "@/components/PageShell";
+
+export default function ResourcesPage() {
+  return (
+    <PageShell
+      title="자료실"
+      description="보고서, 발간물, 참고 자료를 제공합니다. 현재는 데모용 템플릿입니다."
+    />
+  );
+}
